6d2b0eaa91a3e9f47e2a416614a6d7f2e59f927d
githong@kakao.com <rkfaorl12!@>
2021-03-06 14:40:58 +0900

    Thymeleaf 적용

--- src/main/java/com/bale/econproject/EconProjectApplication.java
+++ src/main/java/com/bale/econproject/EconProjectApplication.java
@@ -3,7 +3,9 @@ package com.bale.econproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {"com.bale.econproject", "com.bale.fwk"}
+)
 public class EconProjectApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/bale/fwk/config/BaseConfig.java
@@ -0,0 +1,30 @@
+package com.bale.fwk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+@Configuration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class BaseConfig {
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/views/");
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML");
+        templateResolver.setCacheable(false);
+
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+}
