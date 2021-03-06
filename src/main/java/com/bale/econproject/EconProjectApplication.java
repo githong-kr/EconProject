990b6dbe4fc5a644080244116e85120caa9264c9
githong@kakao.com <rkfaorl12!@>
2021-03-06 13:44:11 +0900

    최초 커밋  ㄴ 프로젝트 생성

---
+++ src/main/java/com/bale/econproject/EconProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bale.econproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EconProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bale/econproject/EconProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bale.econproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
